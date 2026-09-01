02adae6bfd64589bdda345044b6342ccea28612a
Yeetnade <shreyassundar0305@gmail.com>
2026-09-01 16:42:58 -0700

    chore: update package.json with testing and linting scripts, add jest configuration, and update dependencies
    feat: add tsconfig paths for components and assets, enable allowUnreachableCode
    
    chore: create .env.example for Supabase configuration
    
    chore: add Prettier configuration file
    
    chore: add ESLint configuration with Expo and Prettier integration

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig, globalIgnores } = require('eslint/config');
+const expoConfig = require("eslint-config-expo/flat");
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = defineConfig([
+  globalIgnores(['dist/*', '.expo', 'supabase/.temp/*']),
+  expoConfig,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'spaced-comment': ['warn', 'always', { markers: ['/'] }],
+    },
+  },
+]);
